9bb8dc721e801995a0ed44d43fc279ff9c32c80d
OFelipeMeira <felipecmeira2004@gmail.com>
2023-09-14 10:01:20 -0300

    quase api funcionando

--- database.py
+++ database.py
@@ -4,10 +4,7 @@ from pydantic import BaseModel
 def setup_connection():
     con = sqlite3.connect("database.db")
     cursor = con.cursor()
-    cursor.execute("""Create Table Accounts(
-                   id INTEGER PRIMARY KEY AUTOINCREMENT,
-                   name TEXT,
-                   BRL DOUBLE)""")
+    cursor.execute("Create Table Accounts ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, BRL DOUBLE );")
     return {"message": "Connection setted up"}
 
 class Account(BaseModel):
@@ -15,11 +12,12 @@ class Account(BaseModel):
     name:str
     BRL: float
 
-def new_account(name: str, BRL: float):
+#def new_account(name: str, BRL: float):
+def new_account(account:Account):
     con = sqlite3.connect("database.db")
     cursor = con.cursor()
     cursor.execute(f"""Insert Into Accounts(name, BRL)
-                   Values ( '{name}' , '{BRL}')""")
+                   Values ( '{account.name}' , '{account.BRL}')""")
     con.commit()
     con.close()
     return {"message": "Account added with success"}
@@ -27,23 +25,29 @@ def new_account(name: str, BRL: float):
 def get_accounts():
     con = sqlite3.connect("database.db")
     cursor = con.cursor()
-    resp = cursor.execute("select * from Accounts").fetchall()
+    resp = cursor.execute("SELECT * FROM Accounts").fetchall()
     con.close()
     return {"data": resp}
 
-def update_account(account_id:int, new_name:str = "", new_BRL:float = ""):
+def get_account(account_id:int):
+    con = sqlite3.connect("database.db")
+    cursor = con.cursor()
+    resp = cursor.execute(f"SELECT * FROM Accounts WHERE id={account_id}").fetchone()
+    con.close()
+    return {"data": resp}
+
+def get_saldo(account_id:int):
+    con = sqlite3.connect("database.db")
+    cursor = con.cursor()
+    resp = cursor.execute(f"SELECT * FROM Accounts WHERE id={account_id}").fetchone()
+    con.close()
+    return {"data": resp}
+
+def update_account(account: Account):
     con = sqlite3.connect("database.db")
     cursor = con.cursor()
 
-    if new_name == "" and new_BRL=="":
-        return {'ERROR': "No changes new values"}
-
-    if new_name != "":
-        print(f"{new_name}\n")
-        #cursor.execute(f"UPDATE Accounts SET name={new_name} WHERE id={account_id}")
-        cursor.execute(f"UPDATE Accounts SET name='{new_name}'  WHERE id={account_id}")
-    if new_BRL != "":
-        cursor.execute(f"UPDATE Accounts SET BRL ={new_BRL}  WHERE id={account_id}")
+    cursor.execute(f"UPDATE Accounts SET name='{account.name}', BRL={account.BRL}  WHERE id={account.id}")
 
     con.commit()
     con.close()
@@ -59,8 +63,26 @@ def delete_account(account_id:int):
 
 if __name__ == '__main__':
     # print( setup_connection() )
-    # print( new_account("Bob Esponja", 40.0) )
+    
+    # print( new_account("Bo Besponja", 40.4) )
+    # print( new_account("Patrick Trela", 30.6) )
     # print( update_account(1, new_BRL=100) )
-    # print( update_account(1, new_name="abelardo") )
-    # print( get_accounts() ) 
-    print( delete_account(4) )
+    # print( update_account(1, new_name="abelardo", new_BRL=15.5) )
+    # print( delete_account(2) )
+
+    # print( get_account(3) )
+
+    # get all accounts:
+    #for account in get_accounts()["data"]:
+    #   print(account)
+
+    print(
+        get_account(account_id=1)['data'][0]
+    )
+
+    # a  = get_account(1)["data"]
+    # print(len(a))
+
+    
+    pass
+
--- main.py
+++ main.py
@@ -1,32 +1,63 @@
 from fastapi import FastAPI
 import uvicorn
 import database
+import requests
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {'message':"aopa"}
-
+""" WORKS """
 @app.post("/register")
 def create_account(account: database.Account):
     database.new_account(account.name, account.BRL)
-    return {'message': f"{account.name} created"}
+    return {'message': f"Account {account.name} created"}
 
+""" WORKS """
 @app.get("/accounts")
 def get_accounts():
     response = database.get_accounts()
     return {'message':'Data returned', 'data': response['data']}
 
-@app.put("/update/{account_id}")
-def update_account(account_id:int, newName:str = "", newBRL: float =""):
-    database.update_account(account_id=account_id,new_name= newName, new_BRL=newBRL)
-    return {"message": "Account updated", "newValues":[newName, newBRL]}
+""" WORKS """
+@app.get("/account/{account_id}")
+def get_account(account_id:int ):
+    response = database.get_account(account_id=account_id)
+    return {'message':'Data returned', 'data': response['data']}
 
+""" WORKS """
+@app.put("/update")
+def update_account(account: database.Account):
+
+    if len(get_account(account.id)['data']) == 1:
+        database.update_account(account=account)
+        return {"message": "Account updated"}
+    elif len(get_account(account.id)['data']) > 1:
+        return {"error": "too many itens found"}
+    return {'message': "Account id not found"}
+
+""" WORKS """
 @app.delete("/delete/{account_id}")
 def delete_account(account_id:int):
     database.delete_account(account_id=account_id)
     return {"message": "Account deleted"}
+
+""" NOT YET """
+@app.get("/convert/{account_id}")
+async def convert(account_id:int, quota: str = "" ):  
+
+    url = "http://api.exchangeratesapi.io/v1/latest?access_key=ee694924a67ecd528d6cc2288d7cf245&format=2"    
+    response = requests.get(url=url).json()
+
+    saldo_BRL = database.get_saldo(account_id=account_id)
+    valor_BRL = response["rates"]["BRL"]
+
+    if quota == "":
+        saldos = {}
+        for key in response['rates'].keys():
+            saldos[key] = ( response["rates"][key] * saldo_BRL ) / valor_BRL
+        return saldos
+    else:
+        a= response["rates"][quota] * saldo_BRL / valor_BRL
+        return {quota:a}
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
