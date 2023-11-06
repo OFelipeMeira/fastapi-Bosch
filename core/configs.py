b85f80a04df6435dce6378ca529225997ea1fbb7
OFelipeMeira <felipecmeira2004@gmail.com>
2023-11-06 11:04:57 -0300

    configs done

---
+++ core/configs.py
@@ -0,0 +1,28 @@
+"""
+    General Configs File
+"""
+
+from pydantic.v1 import BaseSettings
+from sqlalchemy.orm import declarative_base
+
+class Settings(BaseSettings):
+    """
+        General configs
+    """
+
+    # inicial 'endpoint' - to not be hard coded
+    API_V1_STR: str = '/api/v1'
+
+    # connection string - in this case using root as user, don't need a password
+    # SqlAlchemy template: dialect+driver://username:password@host:port/database
+    DB_URL: str = 'mysql+asyncmy://root@127.0.0.1:3306/etscursos'
+    
+    # for all models inherit all resources from sqlalchemy
+    DBBaseModel = declarative_base()
+
+    # boa pratica sqlalchemy
+    class Config:
+        case_sensitive = True
+
+settings = Settings()
+# importing this file i can have acces to all the configs
--- database.py
+++
@@ -1,9 +0,0 @@
-
-import sqlalchemy as db
-from sqlalchemy import create_engine
-from sqlalchemy import *
-import pymysql
-
-engine = db.create_engine('mysql+pymysql://root:shelvi31@127.0.0.1/errors?host=localhost?port=3306')
-connection = engine.connect()
-print(engine.table_names())
